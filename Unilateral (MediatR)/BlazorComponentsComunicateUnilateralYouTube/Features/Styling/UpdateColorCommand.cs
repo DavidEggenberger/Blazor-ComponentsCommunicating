3f0a753e99473dba480214cc159e9a371c563e38
David Eggenberger <72417712+DavidEggenberger@users.noreply.github.com>
2021-06-21 00:24:34 +0200

    Add files via upload

---
+++ Unilateral (MediatR)/BlazorComponentsComunicateUnilateralYouTube/Features/Styling/UpdateColorCommand.cs
@@ -0,0 +1,41 @@
+﻿using BlazorComponentsComunicateUnilateralYouTube.Features.Styling;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorComponentsComunicateUnilateralYouTube.Features.Styling
+{
+    public class UpdateColorCommand : INotification
+    {
+        public string Color { get; set; }
+    }
+    public class UpdateColorEventArgs : EventArgs
+    {
+        public string Color { get; set; }
+    }
+}
+namespace BlazorComponentsComunicateUnilateralYouTube.Pages
+{
+    public partial class Counter : INotificationHandler<UpdateColorCommand>
+    {
+        public static event EventHandler<UpdateColorEventArgs> ColorChanged;
+        public async Task Handle(UpdateColorCommand notification, CancellationToken cancellationToken)
+        {
+            ColorChanged?.Invoke(this, new UpdateColorEventArgs { Color = notification.Color });
+        }
+    }
+}
+namespace BlazorComponentsComunicateUnilateralYouTube.Shared
+{
+    public partial class MainLayout : INotificationHandler<UpdateColorCommand>
+    {
+        public static event EventHandler<UpdateColorEventArgs> ColorChanged;
+        public async Task Handle(UpdateColorCommand notification, CancellationToken cancellationToken)
+        {
+            ColorChanged?.Invoke(this, new UpdateColorEventArgs { Color = notification.Color });
+        }
+    }
+}
