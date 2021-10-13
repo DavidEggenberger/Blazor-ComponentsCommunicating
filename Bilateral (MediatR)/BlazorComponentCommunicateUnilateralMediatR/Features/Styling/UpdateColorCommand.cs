f553d515254c71e4b4e2cc08aeb6cf2e1996e2b1
David Eggenberger <72417712+DavidEggenberger@users.noreply.github.com>
2021-10-13 17:49:17 +0200

    Add files via upload

---
+++ Bilateral (MediatR)/BlazorComponentCommunicateUnilateralMediatR/Features/Styling/UpdateColorCommand.cs
@@ -0,0 +1,30 @@
+﻿using BlazorComponentCommunicateUnilateralMediatR.Features.Styling;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorComponentCommunicateUnilateralMediatR.Features.Styling
+{
+    public class UpdateColorCommand : INotification
+    {
+        public string Color { get; set; }
+    }
+    public class UpdateColorEventArgs : EventArgs
+    {
+        public string Color { get; set; }
+    }
+}
+namespace BlazorComponentCommunicateUnilateralMediatR.Pages
+{
+    public partial class Counter : INotificationHandler<UpdateColorCommand>
+    {
+        public static event EventHandler<UpdateColorEventArgs> ColorChanged;
+        public async Task Handle(UpdateColorCommand notification, CancellationToken cancellationToken)
+        {
+            ColorChanged?.in
+        }
+    }
+}
